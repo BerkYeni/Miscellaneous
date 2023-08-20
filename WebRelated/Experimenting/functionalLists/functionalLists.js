45fa30eec9d3e7c3e6ffae596affb1495ca0e0b9
BerkYeni <141870638+BerkYeni@users.noreply.github.com>
2023-08-20 23:02:00 +0300

    implemented functional list to array and vice versa conversion functions.

--- WebRelated/Experimenting/functionalLists/functionalLists.js
+++ WebRelated/Experimenting/functionalLists/functionalLists.js
@@ -1,4 +1,25 @@
 const pair = (head) => (tail) => ({ head: head, tail: tail });
 const head = (pair) => pair.head;
 const tail = (pair) => pair.tail;
-const notEqual = (head, tail) => (head !== tail ? [head, tail] : null);
+const notEqual = (head) => (tail) => head !== tail ? [head, tail] : null;
+
+const arrayToFunctionalList = (array) =>
+  array.reduceRight(
+    (previousValue, currentValue) => pair(currentValue)(previousValue),
+    null
+  );
+
+const functionalListToArray = (functionalList) => {
+  const result = [];
+  let handle = functionalList;
+  while (handle !== null) {
+    const { head, tail } = handle;
+    result.push(head);
+    handle = tail;
+  }
+  return result;
+};
+
+const numbers = arrayToFunctionalList([1, 2, 3]);
+console.log("Functional List: ", numbers);
+console.log("Array: ", functionalListToArray(numbers));
